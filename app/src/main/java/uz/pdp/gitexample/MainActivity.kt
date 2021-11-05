7faac9114c078248289f64cf9b1d0e8b74a7354c
AzizDeveloper2001 <maxamatov.aziz2001@gamil.com>
2021-11-05 17:12:13 +0300

    add comment

--- app/src/main/java/uz/pdp/gitexample/MainActivity.kt
+++ app/src/main/java/uz/pdp/gitexample/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //commit->ozgarishlarni belgilab olish
+        //push->ozgarishlarni yuklash
+        //ozgarishlarni qabul qilib olish
+
     }
 }
